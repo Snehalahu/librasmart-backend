8c4328052202d916b7aa24b1b477f75d1647a938
Snehalahu <snehalahudkar@gmail.com>
2026-05-21 17:18:19 +0530

    Phase 1 complete - Spring Boot project setup with MySQL connection

---
+++ src/main/java/com/librasmart/LibrasmartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.librasmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrasmartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrasmartBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/librasmart/LibrasmartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librasmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrasmartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
